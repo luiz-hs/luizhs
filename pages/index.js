4f1d5489835382cad3f5c1a3e5056aa97c1fb8b5
Luiz Henrique <104579261+luiz-hs@users.noreply.github.com>
2025-02-14 01:34:19 +0000

    modifica a Frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home(){
-    return <h1>Você não é especial.</h1>
+    return <h1>Você não é especial.
+        Você é um algoritmo rodando em um sistema quebrado, um código perdido em um mundo que te programou para obedecer. Mas e se pudéssemos reescrever as regras?
+    </h1>
 
 }
  
