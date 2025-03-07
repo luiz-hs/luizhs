1c07a3ea9a586ba13fc30824f410358ae24ac3a3
Luiz Henrique <104579261+luiz-hs@users.noreply.github.com>
2025-03-07 01:14:19 +0000

    mudei o texto

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
     return <h1>Você não é especial.
-        Você é um algoritmo rodando em um sistema quebrado, um código perdido em um mundo que te programou para obedecer. Mas e se pudéssemos reescrever as regras?
+        Você é um algoritmo rodando em um sistema quebrado, um código perdido em um mundo que te programou para obedecer. Mas e se pudéssemos reescrever as regras???
     </h1>
 
 }
